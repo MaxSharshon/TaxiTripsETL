e37cacb7d8eaf1e0b8f557a760b585352386144c
MaxSharshon <sharshonmax@gmail.com>
2026-03-16 21:33:43 +0200

    feat(db): add SQL scripts for TaxiTrips schema

---
+++ sql/create_indexes.sql
@@ -0,0 +1,8 @@
+﻿USE TaxiTripsDb;
+GO
+
+CREATE INDEX IX_TaxiTrips_PULocationID ON TaxiTrips(PULocationID);
+
+CREATE INDEX IX_TaxiTrips_TripDistance ON TaxiTrips(TripDistance DESC);
+
+CREATE INDEX IX_TaxiTrips_Time ON TaxiTrips(PickupDatetime, DropoffDatetime);
---
+++ sql/create_trips_table.sql
@@ -0,0 +1,23 @@
+﻿USE TaxiTripsDb;
+GO
+
+CREATE TABLE TaxiTrips
+(
+    Id INT IDENTITY PRIMARY KEY,
+
+    PickupDatetime DATETIME2 NOT NULL,
+    DropoffDatetime DATETIME2 NOT NULL,
+
+    PassengerCount INT,
+
+    TripDistance FLOAT,
+
+    StoreAndFwdFlag NVARCHAR(3),
+
+    PULocationID INT,
+    DOLocationID INT,
+
+    FareAmount DECIMAL(10,2),
+    
+    TipAmount DECIMAL(10,2)
+);

d326961ca1feb917cc88f85cc5d6e4f4ac38565d
MaxSharshon <sharshonmax@gmail.com>
2026-03-17 13:07:15 +0200

    feat(deduplication): implement duplicate detection logic

---
+++ TaxiTripsETL/Services/DuplicateDetector.cs
@@ -0,0 +1,31 @@
+﻿using TaxiTripsETL.Models;
+
+namespace TaxiTripsETL.Services;
+
+public class DuplicateDetector
+{
+    public IEnumerable<TaxiTrip> Detect(
+        IEnumerable<TaxiTrip> records,
+        Action<TaxiTrip> onDuplicateFound)
+
+    {
+        var seen = new HashSet<(DateTime, DateTime, int?)>();
+
+        foreach (var record in records)
+        {
+            var key = (
+                record.PickupDatetime,
+                record.DropoffDatetime,
+                record.PassengerCount);
+
+            if (!seen.Add(key))
+            {
+                onDuplicateFound(record);
+            }
+            else
+            {
+                yield return record;
+            }
+        }
+    }
+}
---
+++ TaxiTripsETL/Services/TripsCsvWriter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using CsvHelper;
+using TaxiTripsETL.Models;
+
+namespace TaxiTripsETL.Services;
+
+public class TripsCsvWriter : IDisposable
+{
+    private readonly StreamWriter _streamWriter;
+    private readonly CsvWriter _csvWriter;
+
+    public TripsCsvWriter(string filePath)
+    {
+        _streamWriter = new StreamWriter(filePath);
+        _csvWriter = new CsvWriter(_streamWriter, CultureInfo.InvariantCulture);
+    }
+    
+    public void Write(TaxiTrip record)
+    {
+        _csvWriter.WriteRecord(record);
+        _csvWriter.NextRecord();
+    }
+
+    public void Dispose()
+    {
+        _csvWriter.Dispose();
+        _streamWriter.Dispose();
+    }
+}
